a7fc19c917f344f9abdc2aafe040626df306d356
pablozuta <pablo.zuta.h@gmail.com>
2023-04-25 01:21:35 -0400

    inicio repositorio

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 2666;
+    printf("el numero es %d\n", numero);
+
+    char frase[] = "le titanesque monument litteraire";
+    printf("la frase es %s\n", frase);
+  
+
+
+    return 0;
+}
